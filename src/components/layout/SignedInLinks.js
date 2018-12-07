c2d5fad7aaf89728614a451580192aea5359cec4
angelsol299 <angelsol299@hotmail.com>
2018-12-07 23:42:05 +0100

    lay out files

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New Post</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log Out</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating blue accent-4">
+          AA
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
