2ca9aef864e76dfd4fc7f508725613e760ee265c
angelsol299 <angelsol299@hotmail.com>
2018-12-08 18:22:33 +0100

    project details finished'

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,21 +1,23 @@
 import React from "react";
 
-const ProjectDetails = () => {
+const ProjectDetails = props => {
+  console.log(props);
+  const id = props.match.params.id;
   return (
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project</span>
+          <span className="card-title">Project title {id}</span>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem placeat
             ducimus cupiditate? Deleniti odio dolorem modi in soluta deserunt
             eum, quam rerum quas blanditiis delectus laborum voluptas provident
             doloremque sunt?
           </p>
-          <div className="card-cation great lighten-4 grey-text">
-            <div>Posted by Mario Marin</div>
-            <div>2dn december, 2am</div>
-          </div>
+        </div>
+        <div className="card-cation gret lighten-4 grey-text">
+          <div>Posted by Mario Marin</div>
+          <div>2dn december, 2am</div>
         </div>
       </div>
     </div>
