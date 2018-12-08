9785a79b2d7f831d53168667ca45004cf030fcde
angelsol299 <angelsol299@hotmail.com>
2018-12-08 18:11:15 +0100

    project details

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem placeat
+            ducimus cupiditate? Deleniti odio dolorem modi in soluta deserunt
+            eum, quam rerum quas blanditiis delectus laborum voluptas provident
+            doloremque sunt?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
