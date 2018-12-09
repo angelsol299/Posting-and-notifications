8cc134a8d6a87dd6e31e8f570bd5a4387f61a1f1
angelsol299 <angelsol299@hotmail.com>
2018-12-09 18:16:38 +0100

    sign in handles

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,32 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class SignIn extends Component {
-    state={
-
-    }
+  state = {};
+  handleChange = e => {
+    console.log(e);
+  };
+  handleSubmit = e => {
+    console.log(e);
+  };
   render() {
     return (
       <div>
         <div className="container">
-            <form onSubmit={} className="white">
-                <h5 className="grey-test textdarken-3">Sign In</h5>
-                <div className="input-field">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" onChange={}/>
-                </div>
-                <div className="input-field">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" onChange={}/>
-                </div>
-                <div className="input-field">
-                    <button className="btn pinl-a z-depth-0">
-                        Login
-                    </button>
-                </div>
-            </form>
+          <form onSubmit={this.handleSubmit} className="white">
+            <h5 className="grey-test textdarken-3">Sign In</h5>
+            <div className="input-field">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" onChange={this.handleChange} />
+            </div>
+            <div className="input-field">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+              <button className="btn pinl-a z-depth-0">Login</button>
+            </div>
+          </form>
         </div>
       </div>
-    )
+    );
   }
 }
 
