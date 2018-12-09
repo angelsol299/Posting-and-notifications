407a7b8e2e5641d1e4cd0024e5fe2926876956c9
angelsol299 <angelsol299@hotmail.com>
2018-12-09 18:05:17 +0100

    signin

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+    state={
+
+    }
+  render() {
+    return (
+      <div>
+        <div className="container">
+            <form onSubmit={} className="white">
+                <h5 className="grey-test textdarken-3">Sign In</h5>
+                <div className="input-field">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" onChange={}/>
+                </div>
+                <div className="input-field">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" onChange={}/>
+                </div>
+                <div className="input-field">
+                    <button className="btn pinl-a z-depth-0">
+                        Login
+                    </button>
+                </div>
+            </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SignIn;
