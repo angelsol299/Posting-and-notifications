3a893e7062951dce8363509ba81436448f51a129
angelsol299 <angelsol299@hotmail.com>
2018-12-09 18:45:25 +0100

    create project

---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class CreateProject extends Component {
+  state = {
+    Title: "",
+    Content: ""
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <form onSubmit={this.handleSubmit} className="white">
+            <h5 className="grey-test textdarken-3">Sign In</h5>
+            <div className="input-field">
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" onChange={this.handleChange} />
+            </div>
+            <div className="input-field">
+              <label htmlFor="content">Project Content</label>
+              <textarea
+                id="content"
+                className="materialize-textarea"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="input-field">
+              <button className="btn blue-a z-depth-0">Create</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreateProject;
