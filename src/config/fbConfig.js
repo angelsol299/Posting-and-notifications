4ceacb7ceb2a1677270a491dac1908358bd812b6
angelsol299 <angelsol299@hotmail.com>
2018-12-11 21:20:03 +0100

    adding firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyACbml5SG2Gb0YRb4W5PGunqmPM9c7FEHE",
+  authDomain: "recipe-beabf.firebaseapp.com",
+  databaseURL: "https://recipe-beabf.firebaseio.com",
+  projectId: "recipe-beabf",
+  storageBucket: "recipe-beabf.appspot.com",
+  messagingSenderId: "771709045613"
+};
+firebase.initializeApp(config);
