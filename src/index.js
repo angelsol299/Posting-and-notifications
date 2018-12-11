b6fb3e9bbaafd1d1e31dbba1d22c1c8a9f345edc
angelsol299 <angelsol299@hotmail.com>
2018-12-11 21:39:59 +0100

    adding compose

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
@@ -12,7 +12,9 @@ import { getFirebase } from "react-redux-firebase";
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+  )
 );
 
 ReactDOM.render(
