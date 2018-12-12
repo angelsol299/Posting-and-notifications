18577dcb1d05fce2bf0f850ea17e2cc1f3cf309b
angelsol299 <angelsol299@hotmail.com>
2018-12-12 20:37:27 +0100

    adding data to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,10 @@
 export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
+    const firestore = getFirestore();
+
+    firestore.collection("projects").add({});
+
     dispatch({ type: "CREATE_PROJECT", project });
   };
 };
