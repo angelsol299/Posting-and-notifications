4037d161ad7e443a01f02d2cf7d6a2a1712fd4d0
angelsol299 <angelsol299@hotmail.com>
2018-12-12 20:39:19 +0100

    adding user data who is publishing

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -3,7 +3,13 @@ export const createProject = project => {
     // make async call to database
     const firestore = getFirestore();
 
-    firestore.collection("projects").add({});
+    firestore.collection("projects").add({
+      ...project,
+      authorFirstName: "angel",
+      authorLastNname: "osoria",
+      authorId: 1234,
+      createAt: new Date()
+    });
 
     dispatch({ type: "CREATE_PROJECT", project });
   };
