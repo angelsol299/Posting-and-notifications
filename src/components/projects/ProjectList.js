ec51662beebcd9cc3e74964a2a77d3ec1e54c861
angelsol299 <angelsol299@hotmail.com>
2018-12-13 23:58:53 +0100

    adding direction to link

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -7,10 +7,11 @@ const ProjectList = ({ projects }) => {
     <div className="project-list section">
       {projects &&
         projects.map(project => {
-          return;
-          <Link>
-            <ProjectSummary project={project} key={project.id} />;
-          </Link>;
+          return (
+            <Link to={"/project/" + project.id}>
+              <ProjectSummary project={project} key={project.id} />;
+            </Link>
+          );
         })}
     </div>
   );
