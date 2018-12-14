9f8886383059010ab88d95ff947f4b06306d4375
angelsol299 <angelsol299@hotmail.com>
2018-12-14 22:59:14 +0100

    adding signin properties to signin js

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn } from "../../store/actions/authActions";
 
 class SignIn extends Component {
   state = {
@@ -12,7 +14,7 @@ class SignIn extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signIn(this.state);
   };
   render() {
     return (
@@ -34,6 +36,7 @@ class SignIn extends Component {
             </div>
             <div className="input-field">
               <button className="btn blue-a z-depth-0">Login</button>
+              <div className="red-text center" />
             </div>
           </form>
         </div>
@@ -42,4 +45,19 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapStateToProps = state => {
+  return {
+    authorError: state.auth.authError
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signIn: creds => dispatch(signIn(creds))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignIn);
