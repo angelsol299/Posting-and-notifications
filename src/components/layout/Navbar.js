9d15fe516ce21fcd7111a413a947a6fd0f4fdf8c
angelsol299 <angelsol299@hotmail.com>
2018-12-15 17:35:38 +0100

    tracking auth status

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,10 @@ import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
+  console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper blue lighten-1">
       <div className="container">
@@ -19,8 +22,9 @@ const Navbar = () => {
 };
 
 const mapStateToProps = state => {
-  console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth
+  };
 };
 
 export default connect(mapStateToProps)(Navbar);
